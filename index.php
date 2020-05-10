7989cde92b5923f4d290a29ebbaf76cb736070b0
Nuñez Gabriel <angelgabrielnunez1@gmail.com>
2020-05-10 19:30:13 -0300

    Iniciar

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$preguntas = array("Unidad I Estructura
+de un algoritmo","Unidad I Diagramas de flujo","Unidad I Conectores Lógicos","Unidad I Operadores relacionales",
+"Unidad II Desarrollo del ciclo While","Unidad II Desarrollo del ciclo For","Unidad II Desarrollo de condicionales IF","Unidad II CASE","Unidad II
+Variables","Unidad II Variables contadores ","Unidad II Variables de acumulación","Unidad II Tratamiento básico de caracteres","Unidad III Bases de Datos Conceptos y características","Unidad III Sistema de bases de datos: concepto y componentes","Unidad III  Estructura de datos relacional","Unidad III  Conceptos básicos atributos Estructura relacional","Unidad III Conceptos claves","Unidad III Conceptos relaciones","Unidad III  Consultas","Unidad IV Diseño de páginas Web","Unidad IV  Definición de HTML","Unidad IV  Definición de los “tags” básicos: html, head, body, div,etc","Unidad IV  Creación de una página sencilla","Unidad IV  Premisas básicas para la
+creación de una página web","Unidad IV  Etapas en la construcción de una página web","Unidad IV  Prediseño diseño instalación y
+prueba","Unidad IV  Editor de páginas web: Macromedia Dreamweaver o similar:","Unidad IV  Herramientas de
+edición de textos","Unidad IV  Revisión del concepto de hipervínculo Clasificación de los
+hipervínculos","Unidad IV Direcciones absolutas y relativas");
+
+echo "Preguntas 1: ". $preguntas[array_rand($preguntas)];
+echo "<br>Preguntas 2: ". $preguntas[array_rand($preguntas)];
+echo "<br>Preguntas 3: ". $preguntas[array_rand($preguntas)];
+echo "<br>Preguntas 4: ". $preguntas[array_rand($preguntas)];
+echo "<br>Preguntas 5: ". $preguntas[array_rand($preguntas)];
+echo "<br>Preguntas 7: ". $preguntas[array_rand($preguntas)];
+echo "<br>Preguntas 8: ". $preguntas[array_rand($preguntas)];
+echo "<br>Preguntas 9: ". $preguntas[array_rand($preguntas)];
+echo "<br>Preguntas 10: ". $preguntas[array_rand($preguntas)];
+?>
+
+<!-- "Unidad I Conceptos iníciales: Programación modular","Unidad I Programación estructurada ","Unidad I Programación orientada a objetos", -->
